c70b4d88c556612b134968b47db5a4969ab4bfe1
TheMsSalina <salinar@inbox.ru>
2024-08-16 23:11:53 +0300

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,13 @@
+name = 'Радость'
+print(name[0])
+print(name[-1])
+print(name[3:])
+print(name[::-1])
+print(name[0::2])
+
+print('Радость')
+print('Радость'[0])
+print('Радость'[-1])
+print('Радость'[3:])
+print('Радость'[::-1])
+print('Радость'[0::2])
---
+++ Variable.py
@@ -0,0 +1,5 @@
+number_of_completed_hw = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task = (number_of_hours_spent / number_of_completed_hw)
+print('Курс:', course_name, ', всего задач', number_of_completed_hw, ', затрачено часов: ', number_of_hours_spent, ', среднее время выполнения:', time_for_one_task)
