ba80b00eefe2ce4e3fa73b2ced652ffffd4ff456
TheMsSalina <salinar@inbox.ru>
2024-08-23 20:44:18 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,10 +1,9 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 
 l = 0
-if l < len(my_list):
-    while my_list[l] >= 0:
-        if my_list[l] == 0:
-            l += 1
-            continue
-        print(my_list[l])
+while l < len(my_list) and my_list[l] >=0:
+    if my_list[l] == 0:
         l += 1
+        continue
+    print(my_list[l])
+    l += 1
