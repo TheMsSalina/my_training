5b78f2757d11b57d310d18504acae0b043297979
TheMsSalina <salinar@inbox.ru>
2024-08-28 23:05:30 +0300

    Add files via upload

---
+++ module_2_0_hard.py
@@ -0,0 +1,13 @@
+n = int(input("Введите число от 3 до 20: "))
+def get_password(n):
+    password = ''
+    for i in range(1, n):
+        for j in range(2, n):
+            if j <= i:
+                continue
+            if n % (i + j) == 0:
+                password += str(i) + str(j)
+    return password
+
+result = get_password(n)
+print('Пароль:', result)
